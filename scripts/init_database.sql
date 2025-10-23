b4b1e9ce25f39f2a14e4f8e72cc7e1e08ca0e838
stephenadeniyi <adeniyis687@gmail.com>
2025-10-23 13:48:31 +0100

    schemas.sql
    Created three core schemas to organize the data warehouse layers

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,42 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. The script sets up three schemas 
+    in the database: 'bronze', 'silver', and 'gold'.
+	
+NOTE:
+    Running this script will drop the 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted.
+*/
+
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
 
